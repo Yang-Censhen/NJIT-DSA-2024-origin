5a34390f12a24b1c031550fafdad538bec3f9a79
Yang-Censhen <3128504785@qq.com>
2024-03-26 11:14:32 +0800

    DSA-03

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -2,29 +2,28 @@ package oy.tol.tra;
 
 public class Algorithms {
 
-
-
     public static <T> void reverse(T[] array) {
-        int left = 0;
-        int right = array.length - 1;
-        while (left < right) {
-            T temp = array[left];
-            array[left] = array[right];
-            array[right] = temp;
-            left++;
-            right--;
+        int i = 0;
+        int j = array.length - 1;
+        while (i < j) {
+            swap(array, i, j);
+            i++;
+            j--;
         }
     }
 
     public static <T extends Comparable<T>> void sort(T[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - 1 - i; j++) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
                 if (array[j].compareTo(array[j + 1]) > 0) {
-                    T temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+                    swap(array, j, j + 1);
                 }
-            }
-        }
+    }
+
+    public static <T> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -6,8 +6,6 @@ package oy.tol.tra;
  */
 public class Grades {
 
-
-
    private Integer [] grades = null;
 
    /**
@@ -33,13 +31,7 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
       */
-      int i = 0;
-      while (i < grades.length/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-     }
+      Algorithms.reverse(this.grades);
    }
 
    /**
@@ -54,16 +46,7 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
       */
-      int n = grades.length;
-      for (int i = 0;i<n-1;i++) {
-         for (int j=0;j<n-i-1;j++){
-         if (grades[j] > grades[j+1]) {
-            int tmp = grades[j];
-            grades[j] = grades[j+1];
-            grades[j+1] = tmp;
-         }
-         }
-      }
+      Algorithms.sort(this.grades);
    }
 
    /**
