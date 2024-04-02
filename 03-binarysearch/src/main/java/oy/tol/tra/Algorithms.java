ea02a190cd49c162af5efd9950f5606974dea4d5
Yang-Censhen <3128504785@qq.com>
2024-04-02 23:22:13 +0800

    DSA-05

--- 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,81 +1,73 @@
 package oy.tol.tra;
 
 public class Algorithms {
-
     private Algorithms() {
         // Empty
     }
 
-    public static int binarySearch(int result, Integer[] array, int fromIndex, int toIndex){
-        while (fromIndex <= toIndex){
-            int middle = (fromIndex + toIndex)/2;
-            if (array[middle]==result)
-                return middle;
-            else if (array[middle]<result)
-                fromIndex = middle+1;
-            else
-                toIndex = middle-1;
-        }
-        return -1;
-    }
-
-    public static void sort(Integer[] array){
-        boolean swapped;
-        int n=array.length;
-        do {
-            swapped=false;
-            for (int i=1;i<n;i++){
-                if (array[i-1]>array[i]){
-                    int temp=array[i-1];
-                    array[i-1]=array[i];
-                    array[i]=temp;
-                    swapped=true;
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].compareTo(array[j]) > 0) {
+                    T temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                 }
             }
-            n--;
-        }while(swapped);
+        }
     }
 
-    public static int binarySearch(String value, String[] array, int fromIndex, int toIndex){
-        while (fromIndex<=toIndex){
-            int middle=(fromIndex + toIndex)/2;
-            int cpdd=value.compareTo(array[middle]);
-            if (cpdd==0)
+    public static <T> void reverse(T[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            T temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+    }
+
+    public static <T extends Comparable<T>> int binarySearch(T target, T[] array, int fromIndex, int toIndex) {
+        int left = fromIndex;
+        int right = toIndex;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            int cmp = array[middle].compareTo(target);
+            if (cmp == 0) {
                 return middle;
-            else if (cpdd<0)
-                toIndex=middle-1;
-            else
-                fromIndex=middle+1;
+            } else if (cmp < 0) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
         }
         return -1;
     }
 
-    public static void sort(String[] array) {
+    public static <T extends Comparable<T>> void fastSort(T[] array) {
         quickSort(array, 0, array.length - 1);
     }
 
-    private static void quickSort(String[] array,int head,int tail){
-        if (head<tail){
-            int bond = partition(array, head, tail);
-            quickSort(array, head, bond-1);
-            quickSort(array, bond+1, tail);
+    private static <T extends Comparable<T>> void quickSort(T[] array, int head, int tail) {
+        if (head < tail) {
+            int pivotIndex = partition(array, head, tail);
+            quickSort(array, head, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, tail);
         }
     }
 
-    private static int partition(String[] array, int head, int tail) {
-        String htzx=array[tail];
-        int i=head-1;
-        for (int j=head;j<tail;j++) {
-            if (array[j].compareTo(htzx)<0) {
+    private static <T extends Comparable<T>> int partition(T[] array, int head, int tail) {
+        T pivot = array[tail];
+        int i = head - 1;
+        for (int j = head; j < tail; j++) {
+            if (array[j].compareTo(pivot) < 0) {
                 i++;
-                String yxjlm=array[i];
-                array[i]=array[j];
-                array[j]=yxjlm;
+                T tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
             }
         }
-        String yxjlm=array[i+1];
-        array[i+1]=array[tail];
-        array[tail]=yxjlm;
-        return i+1;
+        T tmp = array[i + 1];
+        array[i + 1] = array[tail];
+        array[tail] = tmp;
+        return i + 1;
     }
 }
